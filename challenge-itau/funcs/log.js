f657a8db9a24824d00fcf74020d9f7d3d3f7c3da
lucena-dev <ariane_lucena@outlook.com>
2019-06-04 14:44:38 -0300

    Fix logs

--- challenge-itau/funcs/log.js
+++ challenge-itau/funcs/log.js
@@ -1,16 +1,13 @@
 const fs = require('fs')
 
 function writeLog(description) {
-  fs.statSync('/data/log', function(err, stat) {
-    if(err == null) {
-        fs.appendFileSync("/data/log.txt", description);
-    } else if(err.code === 'ENOENT') {
-      fs.mkdirSync("/data", 0o776);
-      fs.writeFileSync("/data/log.txt", description);
-    } else {
-        console.log('Some other error: ', err.code);
-    }
-  });
+  try {
+    var stats = fs.statSync('funcs/data');
+    fs.appendFileSync("funcs/data/log.txt", description+'\n');
+  } catch(err) {
+    fs.mkdirSync("funcs/data", 0o776);
+    fs.writeFileSync("funcs/data/log.txt", description+'\n');
+  }
 }
 
 module.exports = writeLog;
--- challenge-itau/router.js
+++ challenge-itau/router.js
@@ -4,24 +4,20 @@ const exportLogs = require('./funcs/exportLog');
 function router(request, response) {
   switch (request.method) {
       case "GET":
-      //listar todos os comandos executados;
-      //ler do fylesystem comandos executados;
-      let logs = exportLogs();
-      response.end(logs);
+        let logs = exportLogs();
+        response.end(logs);
+        break;
       case "POST":
-      //chamada da função que deverá abrir um navegador em uma página qualquer
-      //e executar algum código de sua escolha (Ex: código para clicar em um
-      //botão da página ou código para preencher um campo);
-      let e = onBrowser();
-      if (e != '') {
-        response.end("Fail on execute function 'runOnBrowser': ", e)
-      } else {
-        response.end("Running...");
-      }
-      break;
+        let e = onBrowser();
+        if (e != '') {
+          response.end("Fail on execute function 'runOnBrowser': ", e)
+        } else {
+          response.end("Running...");
+        }
+        break;
       default:
-          console.log("unrecognized method: " + request.method);
-          break;
+        console.log("unrecognized method: " + request.method);
+        break;
   }
 }
 
